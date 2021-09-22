7612b6c42d82467976ba85db5acdd6fec3959e8d
WouterAlewijnse <74767434+Wouteralewijnse@users.noreply.github.com>
2021-09-22 12:57:07 +0000

    achtergrond toegevoegd

--- script.js
+++ script.js
@@ -4,26 +4,28 @@ var velocity;
 var mass; 
 var ellipse;
 
+
+var rects = [];
 function setup() {
   createCanvas(640, 360);
-  
 
+
+  bg = loadImage("images/Achtergrond flappy bird.png")
   flappybird = loadImage("images/Flappy bird 2.png");
   yVal = 0;  
   velocity = 0; 
-  mass = 30; 
+  mass = 40; 
   
   accel = mass * 0.01; 
 }
 
 function draw() {
-  background("lightblue");
+  background(bg);
   fill("yellow");
-  
+
   velocity += accel; 
   yVal += velocity;
 	image(flappybird, width/2,yVal, mass,mass); 
-  
   if (yVal > height - mass/2) {
     velocity *= -0.6;
     yVal = height - mass/2;
