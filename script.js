19d5cd2ad9275b3eaa3e3eb3db040ca5159add52
WouterAlewijnse <74767434+Wouteralewijnse@users.noreply.github.com>
2021-09-22 12:21:26 +0000

    Image flappy bird toegevoegd

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ var ellipse;
 function setup() {
   createCanvas(640, 360);
   
+
+  flappybird = loadImage("images/Flappy bird 2.png");
   yVal = 0;  
   velocity = 0; 
   mass = 30; 
@@ -15,12 +17,12 @@ function setup() {
 }
 
 function draw() {
-  background(0);
-  fill(255,0,0);
+  background("lightblue");
+  fill("yellow");
   
   velocity += accel; 
   yVal += velocity;
-	ellipse(width/2,yVal, mass,mass); 
+	image(flappybird, width/2,yVal, mass,mass); 
   
   if (yVal > height - mass/2) {
     velocity *= -0.6;
